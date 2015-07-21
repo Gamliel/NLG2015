3de7d6835a54bbc8936ff21db082d89d2586ca08
Pasquale Iero <pasquale.iero@open.ac.uk>
2015-07-21 22:56:40 +0100

    PI A Really shallow dive

--- ScubaNLG/src/sentence/NLGSentence.java
+++ ScubaNLG/src/sentence/NLGSentence.java
@@ -1,12 +1,16 @@
 package sentence;
 
 public class NLGSentence {
-
+	
+	private double diveDepth;
 	
 	public void setDiveDepth(double d) {
+		diveDepth = d;
 	}
 
 	public String getSentence() {
+		if (diveDepth<=9.6)
+			return "This was a really shallow dive.";
 		return "This was a shallow dive.";
 	}
 
--- ScubaNLG/test/nlg/NLGSentenceTest.java
+++ ScubaNLG/test/nlg/NLGSentenceTest.java
@@ -12,5 +12,11 @@ public class NLGSentenceTest {
 		unit.setDiveDepth(12.0);
 		assertEquals(unit.getSentence(), "This was a shallow dive.");
 	}
-
+	
+	@org.junit.Test
+	public void acceptDiveDepthForAReallyShallowDive(){
+		NLGSentence unit = new NLGSentence();
+		unit.setDiveDepth(9.0);
+		assertEquals(unit.getSentence(), "This was a really shallow dive.");
+	}
 }
