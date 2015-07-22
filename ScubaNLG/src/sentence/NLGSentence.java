e5f494019dc6a4053e066425f11a9eaea4e78bb8
Pasquale Iero <pasquale.iero@open.ac.uk>
2015-07-22 09:01:59 +0100

    PI Safety stop use case added

--- ScubaNLG/src/sentence/NLGSentence.java
+++ ScubaNLG/src/sentence/NLGSentence.java
@@ -1,15 +1,22 @@
 package sentence;
 
+import analytics.DiveletFeatures;
+import analytics.PADITable;
+
 public class NLGSentence {
 	
-	private double diveDepth;
+	private double depth;
+	private DiveletFeatures firstDivelet;
 	
-	public void setDiveDepth(double d) {
-		diveDepth = d;
+	public void setFeatures(double depth, DiveletFeatures firstDiveletFeature) {
+		this.depth = depth;
+		this.firstDivelet = firstDiveletFeature;
 	}
 
 	public String getSentence() {
-		if (diveDepth<=9.6)
+		if(PADITable.needSafetyStop(depth, firstDivelet.getBottomTime()))
+			return "You should have made a safety stop on your way up.";
+		if (depth<=9.6)
 			return "This was a really shallow dive.";
 		return "This was a shallow dive.";
 	}
--- ScubaNLG/test/nlg/NLGSentenceTest.java
+++ ScubaNLG/test/nlg/NLGSentenceTest.java
@@ -2,6 +2,7 @@ package nlg;
 
 import static org.junit.Assert.*;
 
+import analytics.DiveletFeatures;
 import sentence.NLGSentence;
 
 public class NLGSentenceTest {
@@ -9,14 +10,28 @@ public class NLGSentenceTest {
 	@org.junit.Test
 	public void acceptDiveDepthForShallowDive() {
 		NLGSentence unit = new NLGSentence();
-		unit.setDiveDepth(12.0);
+		DiveletFeatures firstDiveletFeature = new DiveletFeatures();
+		firstDiveletFeature.setBottomTime(5);
+		unit.setFeatures(12.0,firstDiveletFeature);
 		assertEquals(unit.getSentence(), "This was a shallow dive.");
 	}
 	
 	@org.junit.Test
 	public void acceptDiveDepthForAReallyShallowDive(){
 		NLGSentence unit = new NLGSentence();
-		unit.setDiveDepth(9.0);
+		DiveletFeatures firstDiveletFeature = new DiveletFeatures();
+		firstDiveletFeature.setBottomTime(5);
+		unit.setFeatures(9.0,firstDiveletFeature);
 		assertEquals(unit.getSentence(), "This was a really shallow dive.");
+	}
+	
+	
+	@org.junit.Test
+	public void acceptShouldHaveMadeAStop(){
+		NLGSentence unit = new NLGSentence();
+		DiveletFeatures firstDiveletFeature = new DiveletFeatures();
+		firstDiveletFeature.setBottomTime(25);
+		unit.setFeatures(85,firstDiveletFeature);
+		assertEquals(unit.getSentence(), "You should have made a safety stop on your way up.");
 	}
 }
