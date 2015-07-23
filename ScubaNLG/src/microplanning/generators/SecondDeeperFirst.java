0b973e5b9b476627725e12effda4bb214c704dce
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2015-07-23 13:25:39 -0300

    Second deeper than first

--- ScubaNLG/src/microplanning/generators/SecondDeeperFirst.java
+++ ScubaNLG/src/microplanning/generators/SecondDeeperFirst.java
@@ -1,11 +1,13 @@
 package microplanning.generators;
 
 import simplenlg.features.Feature;
+import simplenlg.features.Tense;
 import simplenlg.framework.LexicalCategory;
 import simplenlg.framework.NLGElement;
 import simplenlg.framework.NLGFactory;
 import simplenlg.lexicon.Lexicon;
 import simplenlg.phrasespec.NPPhraseSpec;
+import simplenlg.phrasespec.PPPhraseSpec;
 import simplenlg.phrasespec.SPhraseSpec;
 import simplenlg.phrasespec.VPPhraseSpec;
 
@@ -27,10 +29,29 @@ public class SecondDeeperFirst extends Generator{
         predicative.setFeature(Feature.IS_COMPARATIVE, true);
         be.addComplement(predicative);
         
-        
+        PPPhraseSpec than = nlgFactory.createPrepositionPhrase("than");
+        NPPhraseSpec firstDive = nlgFactory.createNounPhrase("one");
+        firstDive.setDeterminer("the");
+        firstDive.setPreModifier("first");
+        than.addComplement(firstDive);
         
         s.setSubject(secondDive);
         s.setVerb(be);
+        s.addComplement(than);
+        s.setFeature(Feature.TENSE, Tense.PAST);
+        
+        SPhraseSpec which = nlgFactory.createClause();
+        VPPhraseSpec beNot = nlgFactory.createVerbPhrase("be");
+        beNot.setFeature(Feature.NEGATED, true);
+        
+        NLGElement recommended = nlgFactory.createAdjectivePhrase("recommended");
+        beNot.addComplement(recommended);
+        beNot.addModifier("really");
+        which.setVerb(beNot);
+        
+        which.setFeature(Feature.COMPLEMENTISER, "which");
+        s.addComplement(which);
+        
 		return s;
 	}
 
